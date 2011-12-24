d623b7326bf688a7aacc483a308266487c3ef404
James Hanlon <jameswhanlon@gmail.com>
2011-12-24 11:02:27 +0000

    First commit.

---
+++ tracesplit.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+
+import sys
+import re
+
+def write(outfiles, core, thread, time, inst):
+  def key(core, thread):
+    return 'c{}t{}'.format(core, thread)
+  k = key(core, thread)  
+  if not k in outfiles:
+    outfiles[k] = open(k, 'w')
+  outfiles[k].write('{} {}\n'.format(time, inst))
+
+def main(args):
+  if len(args) != 2:
+    print('Usage: {} <filename>'.format(args[0]))
+    sys.exit(1)
+  filename = args[1]
+  outfiles = {}
+  pattern = re.compile(r' *([0-9]+) \[c([0-9]+)t([0-9]+)\] (.*)')
+  try:
+    f = open(filename, 'r')
+    line = f.readline()
+    while(line != ''):
+      matcher = re.match(pattern, line)
+      if matcher != None:
+        time = int(matcher.group(1))
+        core = int(matcher.group(2))
+        thread = int(matcher.group(3))
+        inst = matcher.group(4)
+        write(outfiles, core, thread, time, inst)
+        #print('@{} c{}t{}'.format(time, core, thread))
+      line = f.readline()
+    f.close()
+    for k in outfiles.keys():
+      outfiles[k].close()
+  except:
+    raise Exception('Unexpected error: {}'
+        .format(sys.exc_info()[0]))
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
+
